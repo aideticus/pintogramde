d8e765ae010f0c273ca821bb34a3c33730e22637
aideticus <58247775+aideticus@users.noreply.github.com>
2019-12-03 13:47:45 -0300

    pinto.py

--- penis.py
+++ penis.py
@@ -6,53 +6,39 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern='^.penis(?: |$)(.*)')
+@register(outgoing=True, pattern='^.pinto(?: |$)(.*)')
 async def typewriter(typew):
  message = typew.pattern_match.group(1)
- await typew.edit("start loading...")
+ await typew.edit("Crescendo minha rola...")
  sleep(1)
- await typew.edit("0%")
  number = 1
- await typew.edit(str(number) + "%   8D")
- number = number+ 1
+ await typew.edit("8D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8=D")
- number = number+ 1
+ await typew.edit("8=D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8==D")
- number = number+ 1
+ await typew.edit("8==D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8===D")
- number = number+ 1
+ await typew.edit("8===D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8====D")
- number = number+ 1
+ await typew.edit("8====D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8=====D")
- number = number+ 1
+ await typew.edit("8=====D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8======D")
- number = number+ 1
+ await typew.edit("8======D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8=======D")
- number = number+ 1
+ await typew.edit("8=======D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8========D")
- number = number+ 1
+ await typew.edit("8========D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8=========D")
- number = number+ 1
+ await typew.edit("8=========D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8==========D")
- number = number+ 1
+ await typew.edit("8==========D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8===========D")
- number = number+ 1
+ await typew.edit("8===========D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8============D")
- number = number+ 1
+ await typew.edit("8============D")
  sleep(0.50)
- await typew.edit(str(number) + "%   8=============D")
+ await typew.edit("8=============D")
  sleep(1)
- await typew.edit("8=============D Pênis!")
+ await typew.edit("8=============D Gozei")
  # I did it for two hours :D just ctrl+c - crtl+v
