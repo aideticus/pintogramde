a0b91cecf6f46ed92803ff2a8617a1266222936c
aideticus <58247775+aideticus@users.noreply.github.com>
2019-12-03 13:31:25 -0300

    Add files via upload

---
+++ penis.py
@@ -0,0 +1,58 @@
+#made by @DneZyeK
+import asyncio
+import re
+import time
+from time import sleep
+from userbot import CMD_HELP, ZALG_LIST
+from userbot.events import register
+
+@register(outgoing=True, pattern='^.penis(?: |$)(.*)')
+async def typewriter(typew):
+ message = typew.pattern_match.group(1)
+ await typew.edit("start loading...")
+ sleep(1)
+ await typew.edit("0%")
+ number = 1
+ await typew.edit(str(number) + "%   8D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8=D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8==D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8===D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8====D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8=====D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8======D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8=======D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8========D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8=========D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8==========D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8===========D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8============D")
+ number = number+ 1
+ sleep(0.50)
+ await typew.edit(str(number) + "%   8=============D")
+ sleep(1)
+ await typew.edit("8=============D Pênis!")
+ # I did it for two hours :D just ctrl+c - crtl+v
